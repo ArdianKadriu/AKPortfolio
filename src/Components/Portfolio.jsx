ceb934681b2ea9d8fcbf76b42a0a91c3168a4dc0
ArdianKadriu <108486749+ArdianKadriu@users.noreply.github.com>
2024-12-03 12:44:24 +0000

    Updated the project to the right repository

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/DeskPort.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Desk with a laptop, coffee cup, and a plant on the side";
 
 /**
  * Project list
@@ -31,12 +31,6 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
     title: "Web Development for Beginners",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
@@ -45,14 +39,20 @@ const projectList = [
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created from a GitHub template i found working on a course of theirs, this site is a great way to showcase your work and experience.",
+    url: "https://github.com/ArdianKadriu/Portfolio",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "GitHub Codespaces",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "The Codespace course from the Student Developer Pack in GitHub, I included the finished product below.",
+    url: "https://github.com/ArdianKadriu/skills-code-with-codespaces",
+  },
+  {
+    title: "GitHub Introduction",
+    description:
+      "The introduction to the Student Developer Pack course in GitHub, find the completed repo below.",
+    url: "https://github.com/ArdianKadriu/skills-introduction-to-github",
   },
 ];
 
